d73861477b70fc13b2586862ae41296937192174
MrChaos <mrchaos1213@gmail.com>
2024-01-29 16:31:07 +0100

    instanciate the repo

---
+++ an_animal_contest_1_p3_-_happy_alpacas/src/main/java/competitive_coding/Main.java
@@ -0,0 +1,17 @@
+package competitive_coding;
+
+public class Main {
+    public static void main(String[] args) {
+        Main r = new Main();
+        r.run();
+    }
+
+
+    public void run(){
+
+    }
+
+    public String howTheyHappy(){
+        
+    }
+}

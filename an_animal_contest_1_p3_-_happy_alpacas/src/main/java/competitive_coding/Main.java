7fd785323b731ff142585a66ffeef6e3d0f24a5d
MrChaos <mrchaos1213@gmail.com>
2024-01-30 20:30:03 +0100

    Needa learn bitshifting or sth like that

--- an_animal_contest_1_p3_-_happy_alpacas/src/main/java/competitive_coding/Main.java
+++ an_animal_contest_1_p3_-_happy_alpacas/src/main/java/competitive_coding/Main.java
@@ -13,90 +13,101 @@ public class Main {
 
 
     public void run() throws IOException{
-        System.out.println(howTheyHappy());
+        long start = System.currentTimeMillis();
+
+        System.out.println(howTheyHappy().trim());
+        
+        double finish = System.currentTimeMillis();
+        double timeElapsed = finish - start;
+        System.out.println((long)timeElapsed);
     }
 
     public String howTheyHappy() throws IOException{
+        
+        String solution = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         int numberOfAlpacas = Integer.parseInt(st.nextToken());
         int howManyHappy = Integer.parseInt(st.nextToken());
         
-        int[] alpacaNumber = new int[numberOfAlpacas];
-        int checkHappy = 0;
+        //int[] alpacaNumber = new int[numberOfAlpacas];
 
-        if((numberOfAlpacas + 2) % 2 != 0){
-            if((howManyHappy + 2) % 2 == 0){
+        if(isEven(numberOfAlpacas) == true){
+            if(isEven(numberOfAlpacas) == false){
                 return "-1";
             }
         }
 
-        if((howManyHappy + 2) % 2 != 0){
-            if((numberOfAlpacas + 2) % 2 == 0){
+        if(isEven(numberOfAlpacas) == false){
+            if(isEven(numberOfAlpacas) == true){
                 return "-1";
             }
         }
 
         int giveNumberWithHappy = 1;
-        while (checkHappy != howManyHappy - 1 && howManyHappy - 1 >= 1) {         
-            
-            alpacaNumber[checkHappy] = giveNumberWithHappy;
-            switch (giveNumberWithHappy) {
-                case 1:
-                    giveNumberWithHappy = 3;
-                break;
-            
-                case 3:
-                    giveNumberWithHappy = 1;
-                break;
+
+        if(howManyHappy - 1 >= 1){
+            for (int i = 0; i < howManyHappy - 1; i++) {
+                //alpacaNumber[i] = giveNumberWithHappy;
+                solution = solution + String.valueOf(giveNumberWithHappy) + " ";
+                switch (giveNumberWithHappy) {
+                    case 1:
+                        giveNumberWithHappy = 3;
+                    break;
+                
+                    case 3:
+                        giveNumberWithHappy = 1;
+                    break;
+                } 
             }
-            checkHappy++;
+            int numberContinue = 1;
+            for(int i = howManyHappy - 1; i < numberOfAlpacas; i++){
+                //alpacaNumber[i] = numberContinue;
+                solution = solution + String.valueOf(numberContinue) + " ";
+                switch (numberContinue) {
+                    case 1:
+                        numberContinue = 2;
+                    break;
+                
+                    case 2:
+                        numberContinue = 1;
+                    break;
+                }
+            }
         }
+
+        
 
         int giveNumberNoHappy = 1;
         switch (howManyHappy) {
             case 0:
-                for(int i = 0; i < alpacaNumber.length; i++){
-                    alpacaNumber[i] = giveNumberNoHappy;
-                    switch (giveNumberNoHappy) {
-                        case 1:
-                            giveNumberNoHappy = 2;    
-                        break;
-                    
-                        case 2:
-                            giveNumberNoHappy = 1;    
-                        break;
-                    }// entfernbar einfach hochzählen?
+                for(int i = 0; i < numberOfAlpacas; i++){
+                    //alpacaNumber[i] = giveNumberNoHappy;
+                    solution = solution + String.valueOf(giveNumberNoHappy) + " ";
+                    giveNumberNoHappy++;
                 }    
-            break;
+                return solution;
         
-            default:
-                for(int i = checkHappy; i < alpacaNumber.length; i++){
-                    alpacaNumber[i] = giveNumberNoHappy;
-                    switch (giveNumberNoHappy) {
-                        case 1:
-                            giveNumberNoHappy = 2;    
-                        break;
-                    
-                        case 2:
-                            giveNumberNoHappy = 1;    
-                        break;
-                    }
-                }// muss es der letzte sein? (sparen "2. while")
-            break;
+            case 1:
+                for(int i = 0; i < numberOfAlpacas; i++){
+                    //alpacaNumber[i] = giveNumberNoHappy;
+                    solution = solution + String.valueOf(giveNumberNoHappy) + " ";
+                    giveNumberNoHappy++;
+                }
+                return solution;
         }
-
-        String solution = "";
-
-        for(int i = 0; i < alpacaNumber.length - 1; i++){
-            solution = solution + String.valueOf(alpacaNumber[i]) + " ";
-        }// muss es in ein array an Zahlen oder einfach direkt printable machen?
-
-
-        solution = solution + String.valueOf(alpacaNumber[alpacaNumber.length-1]);
-
+        
         return solution;
         
     }
+
+    static boolean isEven (int number)
+  {
+    if((number & 1) == 1){
+        return false;
+    }
+    // n & 1 is 1, then odd, else even
+    return true;
+  }
 }
